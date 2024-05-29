6a21510b303ba10beb96ce83730fa534e6f24a47
Jaewoong Lee <jaewoong.lee@abov.co.kr>
2024-05-29 20:41:22 +0900

    1. Do it Flutter book - Ch12 ex12_1 Commit - List View, List Tile widget

--- 01.Study/do_it_book/Ch12/ex12_01/ex12_01/ex12_01/lib/main.dart
+++ 01.Study/do_it_book/Ch12/ex12_01/ex12_01/ex12_01/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,11 +15,11 @@ class User{
 
 class MyApp extends StatelessWidget {
 
-  List<User> user = [
-    User('홍길동', '010001', 'a@a.com'), User('홍길동', '010001', 'a@a.com'),
-    User('홍길동', '010001', 'a@a.com'), User('홍길동', '010001', 'a@a.com'),
-    User('홍길동', '010001', 'a@a.com'), User('홍길동', '010001', 'a@a.com'),
-    User('홍길동', '010001', 'a@a.com'), User('홍길동', '010001', 'a@a.com'),
+  List<User> users = [
+    User('홍길동', '010001', 'a@a.com'), User('김길동', '010002', 'b@a.com'),
+    User('이길동', '010003', 'c@a.com'), User('박길동', '010004', 'd@a.com'),
+    User('홍길동', '010001', 'a@a.com'), User('김길동', '010002', 'b@a.com'),
+    User('이길동', '010003', 'c@a.com'), User('박길동', '010004', 'd@a.com'),
   ];
   
   @override
@@ -25,10 +27,32 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Test'),
+          title: const Text('Test'),
         ),
-        body: ListTile(
-        ),
+        body: ListView.separated(
+          itemCount: users.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              leading: CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage(
+                  'images/big.jpeg'
+                ),
+              ),
+              title: Text(users[index].name),
+              subtitle: Text(users[index].phone),
+              trailing: Text(users[index].email),
+              onTap: () {
+                print(users[index].name);
+              },
+            );
+          },
+          separatorBuilder: (context, index){
+            return const Divider(
+              height: 2, 
+              color: Colors.black,);
+          },
+          ),
       ),
     );
   }
