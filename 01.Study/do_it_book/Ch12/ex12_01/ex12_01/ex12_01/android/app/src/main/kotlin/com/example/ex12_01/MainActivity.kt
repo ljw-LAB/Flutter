74c763544a8362057910561aea1b68244959a607
ljwlabmac <su5099@naver.com>
2024-07-02 00:53:35 +0900

    1. Do it Flutter book - Ch12 ex12_1 Commit - List View widget

--- 01.Study/do_it_book/Ch12/ex12_01/ex12_01/ex12_01/android/app/src/main/kotlin/com/example/ex12_01/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ex12_01
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ 01.Study/do_it_book/Ch12/ex12_1/android/app/src/main/kotlin/com/example/ex12_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ex12_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
