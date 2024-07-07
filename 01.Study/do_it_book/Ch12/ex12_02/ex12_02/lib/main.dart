4e0d0ed26e60ff9e04d97571ae2f47d0f3ea2a4d
ljwlabmac <su5099@naver.com>
2024-07-08 07:23:28 +0900

    1. Do it Flutter book - Ch12 ex12_02 ~ ex12_04 Commit - Grid View, Page View, Dialog widget

---
+++ 01.Study/do_it_book/Ch12/ex12_02/ex12_02/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  List<String> citys = [
+    '서울시', '인천시', '부산시', '대구시', '대전시', '광주시', '울산시'
+    //
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Test'),
+        ),
+        body: GridView.builder(
+          itemCount: citys.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: Column(
+                children: [
+                  Text(citys[index]),
+                  Image.asset('images/big.jpeg'),
+                ],
+              ),
+            );
+          },
+          scrollDirection: Axis.horizontal,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3),
+        ),
+      ),
+    );
+  }
+}
