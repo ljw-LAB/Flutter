cfe168e708f2bdb80350ddca970261afaa58ae7b
ljwlabmac <su5099@naver.com>
2024-07-17 00:21:51 +0900

    1. Do it Flutter book - Ch14 ex14_03 ~ ex14_04 Commit - Navigation2.0 widget

---
+++ 01.Study/do_it_book/Ch14/ex14_03/ex14_03/android/app/src/main/kotlin/com/example/ex14_03/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ex14_03
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 01.Study/do_it_book/Ch14/ex14_03/ex14_03/lib/main.dart
@@ -0,0 +1,88 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  bool _isDeepLink = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigator Test',
+      home: Navigator(
+        pages: [
+          MaterialPage(
+            child: OneScreen(),
+          ),
+          if (_isDeepLink) MaterialPage(child: TwoScreen()),
+        ],
+        onPopPage: ((route, result) => route.didPop(result)),
+      ),
+    );
+  }
+}
+
+class OneScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('OneScreen'),
+      ),
+      body: Container(
+        color: Colors.red,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'OneScreen',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TwoScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TwoScreen'),
+      ),
+      body: Container(
+        color: Colors.cyan,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'TwoScreen',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Pop')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
